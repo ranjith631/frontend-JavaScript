599becb68ce3d6a4031632969f133d8d28794ee8
ranjith631 <mittaranjith2@gmail.com>
2026-02-27 01:41:12 +0530

    modified

--- day-8/callback.js
+++ day-8/callback.js
@@ -3,16 +3,16 @@
  Write a function called processNumber that accepts a number and a callback function. The processNumber function should double the number and pass the result to the callback function.
 
  */
-// function proccessNumber(num,callback){
-//  var double=num*2;
-//  callback(double);
+function proccessNumber(num,callback){
+ var double=num*2;
+ callback(double);
  
-// }
-// function results(value){
-//     console.log("resluts "+value);
+}
+function results(value){
+    console.log("resluts "+value);
     
-// }
-// proccessNumber(10,results);
+}
+proccessNumber(10,results);
 
 /**
  *     Callback with Array Iteration
@@ -20,16 +20,16 @@
 
  */
 
-//  function forEachElement(array, callback) {
-//   for (let i = 0; i < array.length; i++) {
-//     callback(array[i]);
-//   }
-// }
-// function printelement(element){
-//     console.log(element);   
-// }
-// let arr=[1, 2, 3, 4, 5];
-// forEachElement(arr,printelement);
+ function forEachElement(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
+}
+function printelement(element){
+    console.log(element);   
+}
+let arr=[1, 2, 3, 4, 5];
+forEachElement(arr,printelement);
 
 
 /**
@@ -38,29 +38,29 @@
 
  */
 
-//  function sayHello(name,callback){
-//     setTimeout(() => {
-//         callback("hello "+name);    
-//     }, 1000);
-//  }
-// sayHello("raju",function(message){
-//     console.log(message);
+ function sayHello(name,callback){
+    setTimeout(() => {
+        callback("hello "+name);    
+    }, 1000);
+ }
+sayHello("raju",function(message){
+    console.log(message);
     
-// })
+})
 
 /**
  * 4.          Callback on Condition
  Write a function checkEven that accepts a number and two callback functions. If the number is even, call the first callback with the number. If the number is odd, call the second callback with the number.
 
  */
-// function checkNum(number, evenCallback, oddCallback){
-//     if(number%2===0){
-//         evenCallback(number);
-//     }else{
-//         oddCallback(number);
-//     }
-// }
-// // checkNum(10,num=>console.log(num+"is even"),check);
-// checkNum(11,(num)=>console.log(num+"is even"),(num)=>console.log(num+"is odd"));
+function checkNum(number, evenCallback, oddCallback){
+    if(number%2===0){
+        evenCallback(number);
+    }else{
+        oddCallback(number);
+    }
+}
+// checkNum(10,num=>console.log(num+"is even"),check);
+checkNum(11,(num)=>console.log(num+"is even"),(num)=>console.log(num+"is odd"));
 
 
